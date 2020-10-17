5c34cd22c9959581f236c2e1314140c1435007ad
Kingamattack <kingamattack@outlook.com>
2020-10-17 17:14:40 +0200

    Initial Commit

---
+++ Grocery/GroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryApp.swift
+//  Grocery
+//
+//  Created by Jordy Kingama on 17/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct GroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
